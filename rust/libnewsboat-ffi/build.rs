2d13de90c92858650462526fe81cdd3a144284a9
Alexander Batischev <eual.jp@gmail.com>
2020-09-20 01:32:57 +0300

    Toy with cxxbridge parameters a bit

--- rust/libnewsboat-ffi/build.rs
+++ rust/libnewsboat-ffi/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    cxx_build::bridge("src/utils.rs").compile("cxxbridge-demo");
+    cxx_build::bridge("src/utils.rs").compile("utils");
 
     println!("cargo:rerun-if-changed=src/utils.rs");
 }
