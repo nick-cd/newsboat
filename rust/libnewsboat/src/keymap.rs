5d1cdfc1cfab42d34780f0f198c022618f268161
Alexander Batischev <eual.jp@gmail.com>
2020-10-07 18:33:59 +0300

    Return None on parse error

--- rust/libnewsboat/src/keymap.rs
+++ rust/libnewsboat/src/keymap.rs
@@ -77,13 +77,12 @@ fn operation_sequence(input: &str) -> IResult<&str, Vec<Vec<String>>> {
 /// This function assumes that the input string:
 /// 1. doesn't contain a comment;
 /// 2. doesn't contain backticks that need to be processed.
-pub fn tokenize_operation_sequence(input: &str) -> Vec<Vec<String>> {
+///
+/// Returns `None` if the input could not be parsed.
+pub fn tokenize_operation_sequence(input: &str) -> Option<Vec<Vec<String>>> {
     match operation_sequence(input) {
-        Ok((_leftovers, tokens)) => tokens,
-        Err(error) => {
-            // TODO: handle errors more gracefully, or more in like with tokenize_quoted
-            panic!(format!("{} for input {:?}", error, input));
-        }
+        Ok((_leftovers, tokens)) => Some(tokens),
+        Err(_error) => None,
     }
 }
 
@@ -93,50 +92,57 @@ mod tests {
 
     #[test]
     fn t_tokenize_operation_sequence_works_for_all_cpp_inputs() {
-        assert_eq!(tokenize_operation_sequence(""), Vec::<Vec<String>>::new());
-        assert_eq!(tokenize_operation_sequence("open"), vec![vec!["open"]]);
         assert_eq!(
-            tokenize_operation_sequence("open-all-unread-in-browser-and-mark-read"),
-            vec![vec!["open-all-unread-in-browser-and-mark-read"]]
-        );
-        assert_eq!(
-            tokenize_operation_sequence("; ; ; ;"),
+            tokenize_operation_sequence("").unwrap(),
             Vec::<Vec<String>>::new()
         );
         assert_eq!(
-            tokenize_operation_sequence("open ; next"),
+            tokenize_operation_sequence("open").unwrap(),
+            vec![vec!["open"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence("open-all-unread-in-browser-and-mark-read").unwrap(),
+            vec![vec!["open-all-unread-in-browser-and-mark-read"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence("; ; ; ;").unwrap(),
+            Vec::<Vec<String>>::new()
+        );
+        assert_eq!(
+            tokenize_operation_sequence("open ; next").unwrap(),
             vec![vec!["open"], vec!["next"]]
         );
         assert_eq!(
-            tokenize_operation_sequence("open ; next ; prev"),
+            tokenize_operation_sequence("open ; next ; prev").unwrap(),
             vec![vec!["open"], vec!["next"], vec!["prev"]]
         );
         assert_eq!(
-            tokenize_operation_sequence("open ; next ; prev ; quit"),
+            tokenize_operation_sequence("open ; next ; prev ; quit").unwrap(),
             vec![vec!["open"], vec!["next"], vec!["prev"], vec!["quit"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(r#"set "arg 1""#),
+            tokenize_operation_sequence(r#"set "arg 1""#).unwrap(),
             vec![vec!["set", "arg 1"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(r#"set "arg 1" ; set "arg 2" "arg 3""#),
+            tokenize_operation_sequence(r#"set "arg 1" ; set "arg 2" "arg 3""#).unwrap(),
             vec![vec!["set", "arg 1"], vec!["set", "arg 2", "arg 3"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(r#"set browser "firefox"; open-in-browser"#),
+            tokenize_operation_sequence(r#"set browser "firefox"; open-in-browser"#).unwrap(),
             vec![vec!["set", "browser", "firefox"], vec!["open-in-browser"]]
         );
         assert_eq!(
-            tokenize_operation_sequence("set browser firefox; open-in-browser"),
+            tokenize_operation_sequence("set browser firefox; open-in-browser").unwrap(),
             vec![vec!["set", "browser", "firefox"], vec!["open-in-browser"]]
         );
         assert_eq!(
-            tokenize_operation_sequence("open-in-browser; quit"),
+            tokenize_operation_sequence("open-in-browser; quit").unwrap(),
             vec![vec!["open-in-browser"], vec!["quit"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(r#"open; set browser "firefox --private-window"; quit"#),
+            tokenize_operation_sequence(r#"open; set browser "firefox --private-window"; quit"#)
+                .unwrap(),
             vec![
                 vec!["open"],
                 vec!["set", "browser", "firefox --private-window"],
@@ -144,7 +150,8 @@ mod tests {
             ]
         );
         assert_eq!(
-            tokenize_operation_sequence(r#"open ;set browser "firefox --private-window" ;quit"#),
+            tokenize_operation_sequence(r#"open ;set browser "firefox --private-window" ;quit"#)
+                .unwrap(),
             vec![
                 vec!["open"],
                 vec!["set", "browser", "firefox --private-window"],
@@ -152,7 +159,8 @@ mod tests {
             ]
         );
         assert_eq!(
-            tokenize_operation_sequence(r#"open;set browser "firefox --private-window";quit"#),
+            tokenize_operation_sequence(r#"open;set browser "firefox --private-window";quit"#)
+                .unwrap(),
             vec![
                 vec!["open"],
                 vec!["set", "browser", "firefox --private-window"],
@@ -160,41 +168,42 @@ mod tests {
             ]
         );
         assert_eq!(
-            tokenize_operation_sequence("; ;; ; open",),
+            tokenize_operation_sequence("; ;; ; open",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(";;; ;; ; open",),
+            tokenize_operation_sequence(";;; ;; ; open",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(";;; ;; ; open ;",),
+            tokenize_operation_sequence(";;; ;; ; open ;",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(";;; ;; ; open ;; ;",),
+            tokenize_operation_sequence(";;; ;; ; open ;; ;",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(";;; ;; ; open ; ;;;;",),
+            tokenize_operation_sequence(";;; ;; ; open ; ;;;;",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence(";;; open ; ;;;;",),
+            tokenize_operation_sequence(";;; open ; ;;;;",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence("; open ;; ;; ;",),
+            tokenize_operation_sequence("; open ;; ;; ;",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
-            tokenize_operation_sequence("open ; ;;; ;;",),
+            tokenize_operation_sequence("open ; ;;; ;;",).unwrap(),
             vec![vec!["open"]]
         );
         assert_eq!(
             tokenize_operation_sequence(
                 r#"set browser "sleep 3; do-something ; echo hi"; open-in-browser"#
-            ),
+            )
+            .unwrap(),
             vec![
                 vec!["set", "browser", "sleep 3; do-something ; echo hi"],
                 vec!["open-in-browser"]
@@ -204,38 +213,89 @@ mod tests {
 
     #[test]
     fn t_tokenize_operation_sequence_ignores_escaped_sequences_outside_double_quotes() {
-        assert_eq!(tokenize_operation_sequence(r#"\t"#), vec![vec![r#"\t"#]]);
-        assert_eq!(tokenize_operation_sequence(r#"\r"#), vec![vec![r#"\r"#]]);
-        assert_eq!(tokenize_operation_sequence(r#"\n"#), vec![vec![r#"\n"#]]);
-        assert_eq!(tokenize_operation_sequence(r#"\v"#), vec![vec![r#"\v"#]]);
-        assert_eq!(tokenize_operation_sequence(r#"\""#), vec![vec![r#"\""#]]);
-        assert_eq!(tokenize_operation_sequence(r#"\\"#), vec![vec![r#"\\"#]]);
+        assert_eq!(
+            tokenize_operation_sequence(r#"\t"#).unwrap(),
+            vec![vec![r#"\t"#]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#"\r"#).unwrap(),
+            vec![vec![r#"\r"#]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#"\n"#).unwrap(),
+            vec![vec![r#"\n"#]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#"\v"#).unwrap(),
+            vec![vec![r#"\v"#]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#"\""#).unwrap(),
+            vec![vec![r#"\""#]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#"\\"#).unwrap(),
+            vec![vec![r#"\\"#]]
+        );
     }
 
     #[test]
     fn t_tokenize_operation_sequence_expands_escaped_sequences_inside_double_quotes() {
-        assert_eq!(tokenize_operation_sequence(r#""\t""#), vec![vec!["\t"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\r""#), vec![vec!["\r"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\n""#), vec![vec!["\n"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\v""#), vec![vec!["\x0b"]]); // vertical tab
-        assert_eq!(tokenize_operation_sequence(r#""\"""#), vec![vec!["\""]]);
-        assert_eq!(tokenize_operation_sequence(r#""\\""#), vec![vec!["\\"]]);
+        assert_eq!(
+            tokenize_operation_sequence(r#""\t""#).unwrap(),
+            vec![vec!["\t"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\r""#).unwrap(),
+            vec![vec!["\r"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\n""#).unwrap(),
+            vec![vec!["\n"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\v""#).unwrap(),
+            vec![vec!["\x0b"]] // vertical tab
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\"""#).unwrap(),
+            vec![vec!["\""]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\\""#).unwrap(),
+            vec![vec!["\\"]]
+        );
     }
 
     #[test]
     fn t_tokenize_operation_sequence_passes_through_unsupported_escaped_chars_inside_double_quotes()
     {
-        assert_eq!(tokenize_operation_sequence(r#""\1""#), vec![vec!["1"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\W""#), vec![vec!["W"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\b""#), vec![vec!["b"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\d""#), vec![vec!["d"]]);
-        assert_eq!(tokenize_operation_sequence(r#""\x""#), vec![vec!["x"]]);
+        assert_eq!(
+            tokenize_operation_sequence(r#""\1""#).unwrap(),
+            vec![vec!["1"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\W""#).unwrap(),
+            vec![vec!["W"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\b""#).unwrap(),
+            vec![vec!["b"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\d""#).unwrap(),
+            vec![vec!["d"]]
+        );
+        assert_eq!(
+            tokenize_operation_sequence(r#""\x""#).unwrap(),
+            vec![vec!["x"]]
+        );
     }
 
     #[test]
     fn t_tokenize_operation_sequence_implicitly_closes_double_quotes_at_end_of_input() {
         assert_eq!(
-            tokenize_operation_sequence(r#"set "arg 1"#),
+            tokenize_operation_sequence(r#"set "arg 1"#).unwrap(),
             vec![vec!["set", "arg 1"]]
         );
     }
