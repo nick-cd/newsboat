61fd0674b2b41f96f9e97df156afdf7a11b43cd9
Alexander Batischev <eual.jp@gmail.com>
2020-10-09 16:25:28 +0300

    Fix clippy::boxed_local

--- rust/libnewsboat-ffi/src/scopemeasure.rs
+++ rust/libnewsboat-ffi/src/scopemeasure.rs
@@ -18,6 +18,9 @@ fn create_scopemeasure(scope_name: &str) -> Box<RsScopeMeasure> {
     Box::new(RsScopeMeasure::new(scope_name.to_string()))
 }
 
+// RsScopeMeasure is opaque to C++, so we can only pass it by pointer. Reference won't work as it
+// would prevent us from dropping the object. That leaves Box as the only option.
+#[allow(clippy::boxed_local)]
 fn destroy_scopemeasure(_object: Box<RsScopeMeasure>) {
     // We simply drop the object.
 }
