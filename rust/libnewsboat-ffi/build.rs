5e15a7d4ce6c35152465087ad32e2dcc7ad71d54
Alexander Batischev <eual.jp@gmail.com>
2020-10-10 00:41:03 +0300

    Fix build with GCC {4.5,5} and Clang {4,5}.0

--- rust/libnewsboat-ffi/build.rs
+++ rust/libnewsboat-ffi/build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    cxx_build::bridge("src/utils.rs").compile("utils");
-    cxx_build::bridge("src/scopemeasure.rs").compile("scopemeasure");
+    cxx_build::bridge("src/utils.rs").flag("-std=c++11").compile("utils");
+    cxx_build::bridge("src/scopemeasure.rs").flag("-std=c++11").compile("scopemeasure");
 
     println!("cargo:rerun-if-changed=src/utils.rs");
     println!("cargo:rerun-if-changed=src/scopemeasure.rs");
